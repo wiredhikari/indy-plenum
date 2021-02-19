aeadaa7c46ca5a3a26ff767b30e70d71f3a00fba
Ryan Marsh <ryan.marsh44@gmail.com>
2021-02-19 07:43:41 -0700

    updates setup.py with newer python package versions
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- setup.py
+++ setup.py
@@ -28,12 +28,17 @@ with open(metadata['__file__'], 'r') as f:
     exec(f.read(), metadata)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 tests_require = ['attrs==19.1.0', 'pytest==3.3.1', 'pytest-xdist==1.22.1', 'pytest-forked==0.2',
                  'python3-indy==1.16.0-dev-1636', 'pytest-asyncio==0.8.0']
 =======
 tests_require = ['attrs', 'pytest', 'pytest-xdist', 'pytest-forked',
                  'python3-indy==1.13.0-dev-1420', 'pytest-asyncio']
 >>>>>>> 4ac74853 (upgraded rlp library and moved over some deprecated functionality)
+=======
+tests_require = ['attrs>=20.3.0', 'pytest>=6.2.2', 'pytest-xdist>=2.2.1', 'pytest-forked>=1.3.0',
+                 'python3-indy==1.13.0-dev-1420', 'pytest-asyncio>=0.14.0']
+>>>>>>> e26dcdb9 (updates setup.py with newer python package versions)
 
 
 class PyZMQCommand(distutils.cmd.Command):
@@ -103,6 +108,7 @@ setup(
     # 'rlp==0.5.1',
     install_requires=[
 <<<<<<< HEAD
+<<<<<<< HEAD
                         'jsonpickle==0.9.6',
                         'ujson==1.33',
                         'prompt_toolkit==0.57',
@@ -150,6 +156,30 @@ setup(
                         'pympler',
                         'packaging',
 >>>>>>> 4ac74853 (upgraded rlp library and moved over some deprecated functionality)
+=======
+                        'jsonpickle>=2.0.0',
+                        'ujson>=1.33',
+                        'prompt_toolkit>=3.0.16',
+                        'pygments>=2.2.0',
+                        'rlp>=0.6.0',
+                        'sha3>=0.2.1',
+                        'ioflo>=2.0.2',
+                        'semver>=2.13.0',
+                        'base58>=2.1.0',
+                        'orderedset>=2.0.3',
+                        'sortedcontainers>=1.5.7',
+                        'psutil>=5.6.6',
+                        'pip<10.0.0',
+                        'portalocker>=2.2.1',
+                        'libnacl>=1.7.2',
+                        'six>=1.15.0',
+                        'intervaltree>=2.1.0',
+                        'msgpack-python>=0.5.6',
+                        'python-rocksdb>=0.7.0',
+                        'python-dateutil>=2.8.1',
+                        'pympler>=0.8',
+                        'packaging>=20.9',
+>>>>>>> e26dcdb9 (updates setup.py with newer python package versions)
                         'python-ursa==0.1.1',
                       ],
 
