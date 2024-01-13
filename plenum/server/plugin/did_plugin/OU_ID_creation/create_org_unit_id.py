35d083ac8c1445a8fbab0b2097d991f7c7878feb
PsychoPunkSage <abhinav.prakash319@gmail.com>
2024-01-13 22:39:46 +0530

    fix: DID_Plugin:OU_ID def added

--- plenum/server/plugin/did_plugin/OU_ID_creation/create_org_unit_id.py
+++ plenum/server/plugin/did_plugin/OU_ID_creation/create_org_unit_id.py
@@ -18,6 +18,7 @@ from plenum.common.txn_util import get_payload_data, get_from, \
 
 import libnacl
 import libnacl.encode
+from plenum.server.plugin.did_plugin.request_handlers.create_did_handler import CreateDIDRequest
 
 """
 DID identifier (globally unique)::> Stencil: did:<method-name>:<method-specific-id>
@@ -36,18 +37,93 @@ DID identifier (globally unique)::> Stencil: did:<method-name>:<method-specific-
 }
 
 
-{
-  "DIDDocument": {
-    "id": "did:exampleiin:org1",
-    "verificationMethod": [{
-      "id": "did:exampleiin:org1#key1",
-      "type": "Ed25519VerificationKey2020",
-      "controller": "did:exampleiin:org1",
-      "publicKeyMultibase": "zH3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
-    }],
+================================
+| CreateDID request structure: |
+================================
 
-    "authentication": ["did:exampleiin:org1#key1"]
-  },
-  "signature": "..."
-}
+=================================================================================
+|| {                                                                           ||
+||   "DIDDocument": {   "controller                                            ||
+||     "id": "did:<method-name>:<method-specific-id>",                         ||          
+||     "verificationMethod": [{                                                ||
+||       "id": "did:<method-name>:<method-specific-id>",                       ||
+||       "type": "Ed25519VerificationKey2020",                                 ||
+||       "controller": "did:<method-name>:<method-specific-id>",               ||
+||       "publicKeyMultibase": "zH3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV" ||  <=================??????=======
+||     }],                                                                     ||
+||                                                                             ||
+||     "authentication": ["did:<method-name>:<method-specific-id>"]            ||
+||   },                                                                        ||
+||   "signature": {"did:iin:<method-name>:<method-specific-id>": "..."}        ||    
+|| }                                                                           ||
+=================================================================================
+
+
 """
+
+class CreateOUDIDRequest:
+    did: DID = None
+    did_str = None
+    signature = None
+
+    def __init__(self, request_dict: str) -> None:
+        self.did_str = json.dumps(request_dict["DIDDocument"])
+        self.did = DID(self.did_str)
+        self.signature = request_dict["signature"]
+    
+    def authenticate(self):
+        # Get authentication method
+        auth_method = self.did.fetch_authentication_method(self.signature["verificationMethod"])
+
+        if not auth_method:
+            raise MissingSignature("Authentication verification method not found in DIDDocument.")
+        
+        if auth_method["type"] == "libnacl":
+            # validate signature
+            # TODO: Json serialization is not faithful. Use ordered collections isntead.
+            originalhash = libnacl.crypto_hash_sha256(self.did_str)
+            libnacl_validate(auth_method["publicKeyBase64"], self.signature["sigbase64"], originalhash)
+
+            # TODO: Add more authentication methods / some standard
+        else:
+            raise InvalidSignature("Unknown signature type: ", auth_method["type"])
+
+
+class CreateOUDIDHandler(AbstractDIDReqHandler):
+
+    def __init__(self, database_manager: DatabaseManager, did_dict: dict):
+        super().__init__(database_manager, CREATE_DID, did_dict)
+
+    def additional_dynamic_validation(self, request: Request, req_pp_time: Optional[int]):
+
+        operation = request.operation
+        create_did_request_dict = operation.get(DATA)
+        
+        # parse create did request
+        try:
+            create_did_request = CreateDIDRequest(create_did_request_dict)
+        except:
+            raise InvalidClientRequest(request.identifier, request.reqId, "Malformed CREATE_DID request.")
+
+        # TODO Check if the did uri corresponds to this iin or not.
+
+        # Check if did already in this iin or not.
+        serialized_did = self.state.get(create_did_request.did.id, isCommitted=True)
+        if serialized_did:
+            raise InvalidClientRequest(request.identifier, request.reqId, "DID already exists.")
+
+        # Authenticate
+        create_did_request.authenticate()
+
+
+
+    def update_state(self, txn, prev_result, request, is_committed=False):
+        data = get_payload_data(txn).get(DATA)
+        create_did_request = CreateDIDRequest(data)
+
+        self.did_dict[create_did_request.did.id] = create_did_request.did_str
+        key = create_did_request.did.id
+        val = self.did_dict[create_did_request.did.id]
+        print("Setting state:", key, val)
+        self.state.set(key.encode(), val)
+        return val
